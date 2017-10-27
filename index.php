9b87476115fd48502d20eec1d0b5a7c267f62fab
harrisonhadaway <harrisonleehadaway@gmail.com>
2017-10-27 10:51:27 -0400

    pulling in image and names, need formating

--- index.php
+++ index.php
@@ -14,33 +14,7 @@
 	<h1 class="text-center mt-5">Green Arrow Online</h1>
 	<h3 class="text-center mt-2">Welcome to Team Arrow</h3>
 
-		<div>
-
-		<ul class="list-unstyled">
-  			<li class="media">
-    			<img class="mr-3" src="..." alt="Generic placeholder image">
-   				 <div class="media-body">
-      				<h5 class="mt-0 mb-1">List-based media object</h5>
-      					Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-    			</div>
-  			</li>
-  			<li class="media my-4">
-    			<img class="mr-3" src="..." alt="Generic placeholder image">
-    			<div class="media-body">
-      				<h5 class="mt-0 mb-1">List-based media object</h5>
-      				Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-   				</div>
-  			</li>
-  			<li class="media">
-    			<img class="mr-3" src="..." alt="Generic placeholder image">
-    			<div class="media-body">
-      				<h5 class="mt-0 mb-1">List-based media object</h5>
-      				Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-    			</div>
-  			</li>
-		</ul>
-			
-		</div>
+		
 
 
 <?php
@@ -58,7 +32,29 @@
         // Return a fetch to use the data.
         return pg_fetch_all($request);
     }
-        print_r(getInfo());
+        //print_r(getInfo());
+
+?>
+
+<!-- <ul class="list-unstyled">
+  			<li class="media">
+    			<img class="mr-3" src="..." alt="Generic placeholder image">
+   				 <div class="media-body">
+      				<h5 class="mt-0 mb-1">List-based media object</h5>
+      					Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+    			</div>
+  			</li> -->
+<?php 
+
+	foreach (getInfo() as $heroes) {
+		
+		echo '<li class="media">';
+		echo '<img class="mr-3 img-thumbnail" src="' . $heroes['image_url'] . '" alt="Generic placeholder image">';
+		echo '<div class="media-body">';
+		echo '<h5 class="mt-0 mb-1">' . $heroes['name'] . '</h5>';
+		echo '</div>';
+		echo '</li>';
+	} 
 ?>
 
 <?php //connection for the end of development 
